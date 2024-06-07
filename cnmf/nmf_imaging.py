ac561ede5f855bf7db96e08cc59cc236793f346b
J. Xavier Prochaska <xavier@ucolick.org>
2024-06-07 11:30:36 -0700

    Ren info

--- cnmf/nmf_imaging.py
+++ cnmf/nmf_imaging.py
@@ -1,4 +1,36 @@
-""" Taken from https://github.com/seawander/nmf_imaging """
+""" Taken from https://github.com/seawander/nmf_imaging 
+
+Copyright (c) 2017 Bin Ren
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+DOI: https://doi.org/10.5281/zenodo.3738623
+
+"""
 
 import numpy as np
 import os
@@ -63,7 +95,7 @@ def decolumnize(data, mask):
         return mask_flatten.reshape(mask.shape)
     else:
         #several columns to decolumnize
-        result = np.zeros((data.shape[1], mask.shape[0], mask.shape[1]))
+        result = np.zeros((data.shape[1], mask.sgrouphape[0], mask.shape[1]))
         for i in range(data.shape[1]):
             results_flatten = np.copy(mask_flatten)
             results_flatten[np.where(mask_flatten == 1)] = data[:, i]
