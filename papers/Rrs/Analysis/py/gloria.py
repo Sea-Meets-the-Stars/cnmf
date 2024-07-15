350f98ef2ce207a5a0d37fc275a02420e831c6b6
profxj <xavier@ucolick.org>
2024-07-15 04:48:27 -0700

    GLORIA

--- papers/Rrs/Analysis/py/gloria.py
+++ papers/Rrs/Analysis/py/gloria.py
@@ -1,4 +1,5 @@
 """ Fuss with Gloria """
+import os
 import numpy as np
 
 from matplotlib import pyplot as plt
@@ -10,9 +11,13 @@ from ocpy.insitu import gloria
 from ocpy.utils import spectra
 from ocpy.utils import plotting
 
+from cnmf.oceanography import utils as co_utils
+from cnmf import nmf_imaging
+from cnmf import stats as cnmf_stats
+
 from IPython import embed
 
-def pca_gloria_l23(min_wv:float=400, high_cut:float=700):
+def prep_gloria_l23(min_wv:float=400, high_cut:float=700):
     # Load Gloria
     df_meta, df_Rrs, df_Rrs_std, df_qc_flags = gloria.load_gloria()
 
@@ -28,7 +33,7 @@ def pca_gloria_l23(min_wv:float=400, high_cut:float=700):
     l23_cut = (l23_ds.Lambda > min_wv) & (l23_ds.Lambda < high_cut)
 
     l23_wave = l23_ds.Lambda.data[l23_cut]
-    l23_Rs = l23_ds.Rrs.data[:,l23_cut]
+    l23_Rrs = l23_ds.Rrs.data[:,l23_cut]
 
     # Rebin Gloria to Loisel23
     new_gloria_wv = np.append(l23_wave, [l23_wave.max()+5.]) - 2.5 # Because the rebinning is not interpolation
@@ -37,23 +42,129 @@ def pca_gloria_l23(min_wv:float=400, high_cut:float=700):
 
 
     # Clean up
-    bad_Rs = np.where(np.isnan(gloria_rebin))
+    bad_Rrs = np.where(np.isnan(gloria_rebin))
     keep_gloria = np.ones(gloria_rebin.shape[0], dtype=bool)
-    bad_rows = np.unique(bad_Rs[0])
+    bad_rows = np.unique(bad_Rrs[0])
     keep_gloria[bad_rows] = False
 
     print(f'After all of the cuts, there are {np.sum(keep_gloria)} Gloria spectra left.')
 
+    return l23_Rrs, gloria_rebin[keep_gloria], l23_wave
+
+
+def nmf_gloria_l23(min_wv:float=400, high_cut:float=700, 
+                   N_NMF:int=4):
+
+    l23_Rrs, gloria_rebin, l23_wave = prep_gloria_l23(
+        min_wv, high_cut)
+
+    # Scale for NMF
+    scaled_l23 = l23_Rrs * 1e4
+    scaled_gloria = gloria_rebin * 1e4
+    comb_Rrs = np.vstack((scaled_l23, scaled_gloria))
+
+    # Prep for NMF
+    l23_prepped, l23_mask, l23_err = co_utils.prep(scaled_l23, sigma=0.1)
+    gloria_prepped, gloria_mask, gloria_err = co_utils.prep(scaled_gloria, sigma=0.1)
+    comb_prepped, comb_mask, comb_err = co_utils.prep(comb_Rrs, sigma=0.1)
+
+    # NMF
+    normalize = True
+
+    for spec, mask, err, pref in zip([l23_prepped, gloria_prepped, comb_prepped],
+                               [l23_mask, gloria_mask, comb_mask],
+                               [l23_err, gloria_err, comb_err],
+                               ['L23', 'GLORIA', 'Comb']):
+        outroot = f'{pref}_scaledRrs'
+        for suffix in ['_coef.npy', '_comp.npy']:
+            if os.path.exists(outroot+suffix):
+                os.remove(outroot+suffix)
+        #embed(header='nmf_gloria_l23 79')
+        # NMF
+        comps = nmf_imaging.NMFcomponents(
+            ref=spec, mask=mask, ref_err=err,
+            n_components=N_NMF,
+            path_save=outroot, oneByOne=True,
+            normalize=normalize,
+            seed=12345, verbose=True)
+
+        # Write the data too
+        np.savez(outroot+'_data.npz', spec=spec, 
+                 mask=mask, err=err, wave=l23_wave)
+#
+
+def plot_nmf(comb_only:bool=False):
+
+    outfile = 'nmf_modes.png'
+    if comb_only:
+        outfile = 'nmf_modes_comb.png'
+
+    # Load the NMF components
+    dNMF = {}
+    for ss, pref in enumerate(['L23', 'GLORIA', 'Comb']):
+        outroot = f'{pref}_scaledRrs'
+        dNMF[f'{pref}_M'] = np.load(outroot+'_comp.npy').T
+        dNMF[f'{pref}_coeff'] = np.load(outroot+'_coef.npy').T
+        # 
+        d = np.load(outroot+'_data.npz')
+        dNMF[f'{pref}_spec'] = d['spec'][...,0]
+        # Explained variance
+        dNMF[f'{pref}_evar'] = cnmf_stats.evar_computation(
+            dNMF[f'{pref}_spec'], dNMF[f'{pref}_coeff'], dNMF[f'{pref}_M'])
+        # 
+        print(f'{pref} explained variance: {100*dNMF[f"{pref}_evar"]:0.2f}')
+        #
+        if ss == 0:
+            wave = d['wave']
+
+
+    #embed(header='plot_nmf 117')
+    fig = plt.figure(figsize=(10, 8))
+    ax = plt.gca()
+
+    #embed(header='pca_gloria_l23 77')
+    clrs = ['b', 'g', 'r', 'c', 'm', 'y']
+    for ii in range(dNMF['L23_M'].shape[0]):
+        clr = clrs[ii]
+        if ii == 0:
+            l23_lbl = 'Loisel23'
+            gloria_lbl = 'Gloria'
+        else:
+            l23_lbl = None
+            gloria_lbl = None
+        comb_lbl = f'Comb W{ii+1}'
+        if not comb_only:
+            ax.plot(wave, dNMF['L23_M'][ii], color=clr, ls='--', label=l23_lbl)
+            ax.plot(wave, dNMF['GLORIA_M'][ii], color=clr, ls=':', label=gloria_lbl)
+        ax.plot(wave, dNMF['Comb_M'][ii], color=clr, ls='-', label=comb_lbl)
+
+    # Axes
+    ax.set_xlabel('Wavelength (nm)')
+    ax.set_ylabel('NMF mode')
+    ax.legend(fontsize=10)
+    ax.grid()
+    ax.minorticks_on()
+
+    plotting.set_fontsize(ax, 15)
+    # Save
+    
+    plt.savefig(outfile, dpi=300)
+    print(f'Saved {outfile}')
+
+def pca_gloria_l23(min_wv:float=400, high_cut:float=700):
+
+    l23_Rrs, gloria_rebin, l23_wave = prep_gloria_l23(
+        min_wv, high_cut)
+
     # PCA independently
-    l23_pca_fit = decomposition.PCA(n_components=20).fit(l23_Rs)
+    l23_pca_fit = decomposition.PCA(n_components=20).fit(l23_Rrs)
     gloria_pca_fit = decomposition.PCA(n_components=20).fit(gloria_rebin)
 
     # Combine em too
-    comb_Rrs = np.vstack((l23_Rs, gloria_rebin))
+    comb_Rrs = np.vstack((l23_Rrs, gloria_rebin))
     comb_pca_fit = decomposition.PCA(n_components=20).fit(comb_Rrs)
 
     # Explained variance plot
-
     fig = plt.figure(figsize=(8, 8))
     ax = plt.gca()
 
@@ -83,8 +194,8 @@ def pca_gloria_l23(min_wv:float=400, high_cut:float=700):
     plotting.set_fontsize(ax, 18)
 
     # Save
-    plt.savefig('pca_gloria_l23.png', dpi=300)
-    print(f'Saved pca_gloria_l23.png')
+    plt.savefig('pca_gloria_l23_expvar.png', dpi=300)
+    print('Saved pca_gloria_l23_expvar.png')
 
     # Now let's compare the first 6 PC modes
 
@@ -115,10 +226,15 @@ def pca_gloria_l23(min_wv:float=400, high_cut:float=700):
     plotting.set_fontsize(ax, 15)
     # Save
     plt.savefig('pca_gloria_l23_modes.png', dpi=300)
-    print(f'Saved pca_gloria_l23_modes.png')
+    print('Saved pca_gloria_l23_modes.png')
 
     #embed(header='pca_gloria_l23 77')
  
 
 if __name__ == '__main__':
-    pca_gloria_l23()
+    #pca_gloria_l23()
+
+    # NMF
+    #nmf_gloria_l23()
+    plot_nmf()
+    plot_nmf(comb_only=True)
